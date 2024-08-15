6fea7d72f3530067fac1f846ea4e6008310a93a3
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-08-15 16:54:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let min_cost=0;
+	arr=arr.sort();
+	for(let i=1;i<arr.length;i++){
+		let temp=arr[0];
+		let curr_cost=temp+arr[i];
+		min_cost+=curr_cost;
+		temp=curr_cost;
+		
+	}
   
 }
 
